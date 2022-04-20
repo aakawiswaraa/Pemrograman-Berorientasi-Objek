5f6a7291bb445bc7deb7d5ed1a118ee426a874d2
aakawiswaraa <99965302+aakawiswaraa@users.noreply.github.com>
2022-04-20 22:54:15 +0800

    Create mamalia.java

---
+++ mamalia.java
@@ -0,0 +1,12 @@
+public class mamalia {
+    String nafas="Paru-Paru";
+    String tulang="Vertebrata";
+
+    void bernafas(){
+        System.out.println("mamalia bernafas dengan : "+nafas);
+    }
+    void tulang(){
+        System.out.println("Mamalia berjenis tulang : "+tulang);
+    }
+
+}
